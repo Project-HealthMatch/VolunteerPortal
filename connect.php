c3af93894dd436022094ce4204dbea6cf53a7ecd
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-06 10:43:50 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@ session_start();
      $email = $_POST['email'];
 
 
-     $conn = new mysqli("localhost","root","root","volunteerweb");
+     $conn = new mysqli("healthmatch-server.mysql.database.azure.com","HEALTHMATCH@healthmatch-server","Hackathon2020","volunteerweb");
      if($conn->connect_error)
      {
        die('connection failed :' .$conn->connect_error);
