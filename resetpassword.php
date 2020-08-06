1f15856b8e27c2127c71e15b4dd938609bd027d5
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-06 20:19:44 +0530

    Update resetpassword.php

--- resetpassword.php
+++ resetpassword.php
@@ -25,7 +25,7 @@ else{
 
 if (mysqli_query($conn, $sql))
   {  echo '<script>alert("Password Update Successfully!")</script>';
-   header("Location"dashboard.php");
+   header("Location:dashboard.php");
 
   }
 else {
