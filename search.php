68373643719278e34a4414c16b5e4f0d270f8f91
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-08 20:58:09 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1437,6 +1437,20 @@ body {
   /* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
 }
               }
+              
+              body {
+  background-image: linear-gradient(-225deg, #e3fdf5 0%, #ffe6fa 100%);
+  background-image: linear-gradient(to top, #d2bde4 0%, #a325dd 100%);
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+
+  font-family: "Vibur", cursive;
+  /*   the main font */
+  font-family: "Abel", sans-serif;
+  opacity: 0.95;
+  /* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
+}
+
 
                   </style>
 
