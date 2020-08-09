7e75ed93cb62a89b93478a3030d68f0619690732
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-09 20:06:59 +0530

    Update connecti.php

--- connecti.php
+++ connecti.php
@@ -310,7 +310,8 @@ body {
      $b;
 
 
-  $conn = new mysqli("healthmatch-server.mysql.database.azure.com","HEALTHMATCH@healthmatch-server","Hackathon2020","volunteerweb");
+
+     $conn = new mysqli("healthmatch-server.mysql.database.azure.com","HEALTHMATCH@healthmatch-server","Hackathon2020","volunteerweb");
    if($conn->connect_error)
      {
        die('connection failed :' .$conn->connect_error);
