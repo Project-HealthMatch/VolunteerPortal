e621cf2a2a9a89fc622cdcf33fd5823ba2f047c6
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-09 14:32:48 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,11 +4,11 @@ session_start();
      $date= $_POST['date'];
      $timeZoneId = $_POST['timeZoneId'];
      $slot = $_POST['slot'];
-     $Languages = $_POST['Languages'];
+    
      $email = $_POST['email'];
 
 
-     $conn = new mysqli("healthmatch-server.mysql.database.azure.com","HEALTHMATCH@healthmatch-server","Hackathon2020","volunteerweb");
+     $conn = new mysqli("healthmatch-server.mysql.database.azure.com","HEALTHMATCH@healthmatch-server","Hackathon2020","slotbooking");
      if($conn->connect_error)
      {
        die('connection failed :' .$conn->connect_error);
@@ -17,12 +17,12 @@ session_start();
      else
 
      {
-       $stmt = $conn->prepare("insert into volunteerweb(date, timeZoneId, slot, Languages, email)
+       $stmt = $conn->prepare("insert into volunteerweb(date, timeZoneId, slot, email)
        values(?, ?, ?, ?, ?)");
 
       $b=implode(",",$slot);
       $w=implode(",",$Languages);
-       $stmt->bind_param("sssss",$date,$timeZoneId, $b, $w, $email);
+       $stmt->bind_param("ssss",$date,$timeZoneId, $b,  $email);
        $stmt->execute();
        echo "slot booked";
       header("Location:submitted.php");
