2cc5a986698b9dfac92360d454d5dd5072c75435
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-10 10:29:00 +0530

    Update request.php

--- request.php
+++ request.php
@@ -101,8 +101,7 @@ input{
 }
 
 body {
-       background-image: linear-gradient(-225deg, #e3fdf5 0%, #ffe6fa 100%);
-      background-image: linear-gradient(to top, #adece9 0%, #9ba0da 100%);
+     background-image: linear-gradient(to top, #dfc2f8 0%, #c2dcee 100%);
         background-attachment: fixed;
         background-repeat: no-repeat;
 
